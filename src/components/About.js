291dd03440b064c18eaa6110c878d2239878739d
marcusicaro <marcusicaromc@gmail.com>
2023-02-15 15:20:03 -0300

    liga working on about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion, useInView } from "framer-motion";
 
 export default function About() {
+  const [showText, setShowText] = useState('none')
+
   return (
     <div className='aboutContainer'>
       <h2 className='titleAbout'>
@@ -22,14 +24,18 @@ export default function About() {
               are some of the courses you'll find as soons as you join this
               family. <br />
               <br />
+              </p>
+              <button 
+              onClick={() => {showText === 'block' ? setShowText('none') : setShowText('block') }}>Show more</button>
+              <div style={{display: showText, height:0 }}>   
               Every course comes with rhythm lessons for you to learn the
               different ways to play
               <b>
                 {" "}
                 samba, bossa nova, forró, and all the genres that we are going
-                to play
-              </b>
-              . Talking about brazilian music is talking about rhythm and that's
+                to play.
+              </b>           
+               Talking about brazilian music is talking about rhythm and that's
               a subject that the Brazilian Guitar Academy handles very
               carefully. It is important that you sound like a brazilian guitar
               player. <br />
@@ -37,7 +43,9 @@ export default function About() {
               By the way, all the 7 Stringed guitar courses are adapted for 6
               stringed guitar. <b>Watch it anywhere, anytime, in any order!</b>
               Welcome to the Brazilian Guitar Academy!
-            </p>
+              </div>
+
+
           </div>
           <motion.div
             initial={{ x: 100 }}
