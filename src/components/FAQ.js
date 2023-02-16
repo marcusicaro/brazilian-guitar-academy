ec0c89f370ce822b8d98877b11d4d751c71f3611
Marcus <marcusicaromc@gmail.com>
2023-02-15 21:25:10 -0300

    working on faq

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
+import faqData from "./FAQdata";
+
+import "./faq.css";
 
 export default function FAQ() {
-  return <div>FAQ</div>;
+  const [selected, setSelected] = useState(null);
+
+  function handleToggle(i) {
+    selected === i ? setSelected(null) : setSelected(i);
+  }
+
+  return (
+    <div className='wrapper'>
+      <div className='accordion'>
+        {faqData.map((item, i) => (
+          <div className='item'>
+            <div className='title'>
+              {item.question}
+              <span onClick={() => handleToggle(i)}>
+                {selected === i ? "+" : "-"}
+              </span>
+            </div>
+            <div
+              className='content'
+              style={{ display: selected === i ? "block" : "none" }}
+            >
+              {item.answer}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/components/FAQdata.js
@@ -0,0 +1,20 @@
+const faqData = [
+  {
+    question: "first question",
+    answer: "first answer lorem ipsum sajk hkjas hja shkj ashkjs hjaskhas kjh",
+  },
+  {
+    question: "second question",
+    answer: "first answer lorem ipsum sajk hkjas hja shkj ashkjs hjaskhas kjh",
+  },
+  {
+    question: "third question",
+    answer: "first answer lorem ipsum sajk hkjas hja shkj ashkjs hjaskhas kjh",
+  },
+  {
+    question: "fourth question",
+    answer: "first answer lorem ipsum sajk hkjas hja shkj ashkjs hjaskhas kjh",
+  },
+];
+
+export default faqData;
